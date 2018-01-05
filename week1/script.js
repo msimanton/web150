8edd2f12b81569072d5972ac1dc90fbb2c0b6300
msimanton <matthew.simanton@seattlecentral.edu>
2018-01-05 00:19:11 -0800

    added comments

--- week1/script.js
+++ week1/script.js
@@ -1,7 +1,9 @@
+//define variables
 var firstName = "Matthew";
 var lastName = "Simanton";
 var yearsOfStudy = "1 year";
 var goal = "become a data scientist";
 
+//write to screen
 document.write("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses.");  
     
