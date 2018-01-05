cc6f47ce758e052d33ddb930951705910b437293
msimanton <matthew.simanton@seattlecentral.edu>
2018-01-05 00:03:35 -0800

    final version of Assignment 1

--- week1/script.js
+++ week1/script.js
@@ -1,5 +1,7 @@
 var firstName = "Matthew";
 var lastName = "Simanton";
 var yearsOfStudy = "1 year";
-var goal = "goal";
+var goal = "become a data scientist";
 
+document.write("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses.");  
+    
