fc98d3a3755e004b6da68ac6fd713f3a90f337e4
msimanton <matthew.simanton@seattlecentral.edu>
2018-01-19 16:03:29 -0800

    Added notes to clarify code

--- Assignment3/StudentData.js
+++ Assignment3/StudentData.js
@@ -12,7 +12,7 @@ var grades = "jim|25, sue|32, mary|34, ann|22, ted|28, frank|15, lisa|19, mike|3
 var individual = grades.split(", ");
 individual.sort();
 
-
+//Variable set up
 var total = 0;
 var lowGrade = 100;
 var highGrade = 0;
@@ -21,25 +21,27 @@ var tempArray = [];
 //Parse through the individual data with a for loop
 for (n in individual){
     
-    
+    //Collects the student name
     dataPerson = individual[n].split("|");
     var studentName = dataPerson[0];
+    
+    //Setting up grade as an integer
     var studentGrade = dataPerson[1];
     var temp = parseInt(studentGrade);
     total += temp;
-    
-    
     tempArray.push(temp);
     
+    //check for high and low grade
     if (temp > highGrade) {
         highGrade = temp;
     } else if (temp < lowGrade) {
         lowGrade = temp;
     }
     
+    //Writes to screen the student name and student grade in a list
     document.write("<li>" + studentName[0].toUpperCase() + studentName.slice(1) + "  -  " + studentGrade);    
 }
 
-
+//Writes to screen the Average, High and Low Grades
 document.write("<br>" + "Average: " + total/individual.length + "<br>" + "Highest Grade: " + highGrade + "<br>" + "Lowest Grade: " + lowGrade);
 
